3f52f9d20401973db7fd82d076e4d9b0aafab164
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2023-03-24 08:13:53 +0100

    clase77

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
@@ -12,6 +13,8 @@ export const GifExpertApp = () => {
     return (
         <>
             <h1>GifExpertApp</h1>
+
+            <AddCategory />
 
             <button onClick={onAddCategory}>Agregar</button>
             <ol>
